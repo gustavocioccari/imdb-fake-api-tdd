0bcf586c042c5a324606a39acc4e48317252b05d
gustavocioccari <gustavocioccari@gmail.com>
2021-03-12 18:03:29 -0300

    implement user creation unit test

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const bcrypt = require('bcryptjs')
+
+const factory = require('../utils/factories')
+const truncate = require('../utils/truncate')
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate()
+  })
+
+  it('should encrypt user password', async () => {
+    const user = await factory.create('User', {
+      password: '12345'
+    })
+
+    const compareHash = await bcrypt.compare('12345', user.password_hash)
+    expect(compareHash).toBe(true)
+  })
+})
