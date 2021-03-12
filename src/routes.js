8776aaa8daddcfd572d9d8b26aeb1647efaed3de
gustavocioccari <gustavocioccari@gmail.com>
2021-03-12 18:06:36 -0300

    implements auth controller and auth route

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
+const AuthController = require('./controllers/AuthController')
+
 routes.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+routes.post('/auth', AuthController.store)
 
 module.exports = routes
