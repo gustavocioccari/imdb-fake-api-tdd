9d1c83248a9882fc4e3d4559fa02f2a457c18267
gustavocioccari <gustavocioccari@gmail.com>
2021-03-12 18:04:48 -0300

    add password virtual, password hashing, checkpassword and generatetoken

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,31 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
     password_hash: DataTypes.STRING,
     role: DataTypes.ENUM('admin', 'user'),
     is_active: DataTypes.BOOLEAN
-  })
+  },
+  {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 10)
+        }
+      }
+    }
+  }
+  )
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+  User.prototype.generateToken = function () {
+    return jwt.sign({ id: this.id }, process.env.APP_SECRET)
+  }
+
   return User
 }
