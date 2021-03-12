140707c62b327dc2732db5516545247c40d1df59
gustavocioccari <gustavocioccari@gmail.com>
2021-03-12 13:58:04 -0300

    configure test environment

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
 module.exports = {
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'postgres',
-  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  storage: './__tests__/database.sqlite',
+  logging: false,
   define: {
     underscored: true,
     underscoredAll: true,
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const routes = require('./routes')
 const cors = require('cors')
 const app = express()
 
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
 app.use(cors())
 app.use(express.json())
 app.use(routes)
